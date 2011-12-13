86d59ec49c759479f3d577e4d4c6f15a1e81a3a3
Mateus Freira dos Santos <mateus.freira@gmail.com>
2011-12-13 10:46:25 -0200

    incluindo methodo de save...

--- src/com/mateusfreira/DrawView.java
+++ src/com/mateusfreira/DrawView.java
@@ -1,76 +1,99 @@
 package com.mateusfreira;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.os.Environment;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
 public class DrawView extends View implements OnTouchListener {
-	private static final String TAG = "DrawView";
+    private static final String TAG = "DrawView";
 
-	List<Point> points = new ArrayList<Point>();
-	List<Integer> spases = new ArrayList<Integer>();
-	Paint paint = new Paint();
+    List<Point> points = new ArrayList<Point>();
 
-	public DrawView(Context context) {
-		super(context);
-		setFocusable(true);
-		setFocusableInTouchMode(true);
+    List<Integer> spases = new ArrayList<Integer>();
 
-		this.setOnTouchListener(this);
+    Paint paint = new Paint();
 
-		paint.setColor(Color.WHITE);
-		paint.setAntiAlias(true);
-	}
+    public DrawView(Context context) {
+        super(context);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
 
-	@Override
-	public void onDraw(Canvas canvas) {
-		Point previous = null;
-		int i = 0;
-		for (Point point : points) {
+        this.setOnTouchListener(this);
 
-			if (previous != null && !spases.contains(i)) {
-				canvas.drawLine(previous.x, previous.y, point.x, point.y, paint);
-			} else {
-				canvas.drawCircle(point.x, point.y, 1, paint);
-			}
-			previous = point;
-			i++;
-		}
-	}
+        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+    }
 
-	public boolean onTouch(View view, MotionEvent event) {
-		if (event.getAction() == MotionEvent.ACTION_UP) {
-			spases.add(points.size());
-		} else {
-			Point point = new Point();
-			point.x = event.getX();
-			point.y = event.getY();
-			points.add(point);
-			invalidate();
-		}
-		return true;
-	}
+    @Override
+    public void onDraw(Canvas canvas) {
+        Point previous = null;
+        int i = 0;
+        for (Point point : points) {
 
-	public void clear() {
-		points = new ArrayList<Point>();
-		spases =  new ArrayList<Integer>();
-		invalidate();
-	}
+            if (previous != null && !spases.contains(i)) {
+                canvas.drawLine(previous.x, previous.y, point.x, point.y, paint);
+            } else {
+                canvas.drawCircle(point.x, point.y, 1, paint);
+            }
+            previous = point;
+            i++;
+        }
+    }
+
+    public boolean onTouch(View view, MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            spases.add(points.size());
+        } else {
+            Point point = new Point();
+            point.x = event.getX();
+            point.y = event.getY();
+            points.add(point);
+            invalidate();
+        }
+        return true;
+    }
+
+    public void clear() {
+        points = new ArrayList<Point>();
+        spases = new ArrayList<Integer>();
+        invalidate();
+    }
+
+    public void save() {
+        Log.d(TAG, "Salvo");
+        this.setDrawingCacheEnabled(true);
+        Bitmap b = this.getDrawingCache();
+        try {
+            File sdCard = Environment.getExternalStorageDirectory();
+            File file = new File(sdCard, "image_tela.jpg");
+            FileOutputStream fos = new FileOutputStream(file);
+            b.compress(CompressFormat.JPEG, 95,fos);
+        } catch (FileNotFoundException e) {
+            Log.e(TAG, "erro", e);
+        }
+
+    }
 }
 
 class Point {
-	float x, y;
+    float x, y;
 
-	@Override
-	public String toString() {
-		return x + ", " + y;
-	}
+    @Override
+    public String toString() {
+        return x + ", " + y;
+    }
 }
